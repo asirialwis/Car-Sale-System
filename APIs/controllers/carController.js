5bad32f67e292f4bf27293e7444395e703b2b696
asirialwis <asirichathurika900@gmail.com>
2024-01-08 01:10:15 +0530

    search add

--- APIs/controllers/carController.js
+++ APIs/controllers/carController.js
@@ -1,24 +1,39 @@
 const Car = require("./../models/carDetails");
 
 const addCar = async (req, res) => {
-        const carNumber = req.body.carNumber;
-        const carModel = req.body.carModel;
-        const madeYear = Number(req.body.madeYear);
-    
-        const newCar = new Car({
-            carNumber,
-            carModel,
-            madeYear,
-        });
-    
-        await newCar
-            .save()
-            .then(() => {
-                res.json("car added");
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+  const carNumber = req.body.carNumber;
+  const carModel = req.body.carModel;
+  const madeYear = Number(req.body.madeYear);
+
+  const newCar = new Car({
+    carNumber,
+    carModel,
+    madeYear,
+  });
+
+  await newCar
+    .save()
+    .then(() => {
+      res.json("car added");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
-module.exports = { addCar };
+const getCar = async (req, res) => {
+  let searchmodel = req.params.carModel;
+
+  const result = await Car.collection
+    .find({ carModel: { $regex: searchmodel, $options: "i" } })
+    .toArray();
+
+  
+  if (result) {
+    res.status(200).json(result);
+  } else {
+    res.status(400).json("Car not found");
+  }
+};
+
+module.exports = { addCar, getCar };
--- APIs/routes/Cars.js
+++ APIs/routes/Cars.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const carController = require("./../controllers/carController");
 
 router.route("/addcar").post(carController.addCar);
+router.route("/getcar/:carModel").get(carController.getCar);
 
 module.exports = router;
